c24746a3f8bd38628e91a445e0ed431d1fadc80f
Theepan09 <theepankumar09@live.com>
2024-09-13 14:59:49 +0800

    new commit 2

--- src/Component/Portfolio.js
+++ src/Component/Portfolio.js
@@ -1,9 +1,7 @@
-// src/Component/Portfolio.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ImageModal from './ImageModal'; // Import the ImageModal component
-//import './Portfolio.css'; // Import the CSS for portfolio
-
+import './Portfolio.css'; // Import the CSS for portfolio
 
 const Portfolio = () => {
   const [images, setImages] = useState([]);
@@ -15,14 +13,13 @@ const Portfolio = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const UNSPLASH_ACCESS_KEY = '6Fthq51PIMQQy0VquVJYrjmQlCsBgRKsww5Zvua5PYs';
-  const UNSPLASH_API_URL = 'https://api.unsplash.com/photos/?client_id=' + UNSPLASH_ACCESS_KEY;
-  const UNSPLASH_SEARCH_API_URL = 'https://api.unsplash.com/search/photos/page=1&per_page=30&query=${inputValue}&client_id=' + UNSPLASH_ACCESS_KEY;
+  const UNSPLASH_API_URL = `https://api.unsplash.com/photos/?client_id=${UNSPLASH_ACCESS_KEY}`;
+  const UNSPLASH_SEARCH_API_URL = `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${searchQuery}&client_id=${UNSPLASH_ACCESS_KEY}`;
 
   useEffect(() => {
     const fetchImages = async () => {
       try {
         const response = await axios.get(UNSPLASH_API_URL);
-        console.log(response.data)
         setImages(response.data);
         setFilteredImages(response.data); // Initialize filteredImages with all images
         setLoading(false);
@@ -36,16 +33,20 @@ const Portfolio = () => {
   }, []);
 
   useEffect(() => {
-    // Filter images based on search query
-    if (searchQuery === '') {
-      setFilteredImages(images);
-    } else {
-      const lowercasedQuery = searchQuery.toLowerCase();
-      const filtered = images.filter(image =>
-        image.alt_description?.toLowerCase().includes(lowercasedQuery)
-      );
-      setFilteredImages(filtered);
-    }
+    const fetchFilteredImages = async () => {
+      if (searchQuery === '') {
+        setFilteredImages(images);
+      } else {
+        try {
+          const response = await axios.get(UNSPLASH_SEARCH_API_URL);
+          setFilteredImages(response.data.results);
+        } catch (err) {
+          setError(err);
+        }
+      }
+    };
+
+    fetchFilteredImages();
   }, [searchQuery, images]);
 
   const handleSearchChange = (event) => {
@@ -89,13 +90,13 @@ const Portfolio = () => {
       <br />
       
       <div className="container">
-        <div className="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
+        <div className="portfolio-grid">
           {/* Portfolio Items */}
-          <div className="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
+          <div className="row gy-4">
             {filteredImages.map((image) => (
-              <div key={image.id} className="col-lg-4 col-md-6 portfolio-item isotope-item" >
+              <div key={image.id} className="col-lg-4 col-md-6 portfolio-item">
                 <div className="portfolio-content h-100" onClick={() => openModal(image)}>
-                  <img src={image.urls.small} className="img-fluid" alt={image.alt_description}  />
+                  <img src={image.urls.small} className="img-fluid" alt={image.alt_description} />
                 </div>
               </div>
             ))}
